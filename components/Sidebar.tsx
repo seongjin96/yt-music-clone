ff66bfeae71923a1f2b19676c5da774734447b4c
seongjin96 <w8348@naver.com>
2024-05-27 11:53:39 +0900

    set Sidebar component type

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,8 @@
 import Logo from "@/components/elements/Logo";
 import Navigator from "@/components/elements/Navigator";
+import React from "react";
 
-const Sidebar = ({ children }) => {
+const Sidebar = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-row h-full">
       <nav className="w-[240px] border-r-[1px] border-neutral-600">
