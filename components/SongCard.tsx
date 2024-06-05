9a675b1191acd9d426011ed1de0af14627a7ac54
seongjin96 <w8348@naver.com>
2024-06-05 12:17:26 +0900

    complete SongCard Compnent

--- components/SongCard.tsx
+++ components/SongCard.tsx
@@ -1,8 +1,12 @@
+"use client"
+
 import {TopSong} from "@/types";
 import React from "react";
 import Image from "next/image";
 import {AiOutlineCaretDown, AiOutlineCaretUp} from "react-icons/ai"
 import {FaCircle} from "react-icons/fa";
+import {FiMoreVertical, FiPlayCircle, FiThumbsDown, FiThumbsUp} from "react-icons/fi";
+import IconButton from "@/components/elements/IconButton";
 
 interface SongCardProps {
   song: TopSong;
@@ -10,19 +14,35 @@ interface SongCardProps {
 
 const SongCard: React.FC<SongCardProps> = ({ song }) => {
   return (
-    <article className="flex flex-row gap-4 h-[48px] w-[400px] ">
+    <article className="flex flex-row gap-4 h-[48px] w-full relative group">
       <div className="w-[48px] h-[48px] relative" >
         <Image src={song.imageSrc} alt="img" fill className="object-cover" />
+        <section className="hidden group-hover:flex absolute top-0 w-[48px] h-[48px] items-center justify-center bg-black cursor-pointer">
+          <FiPlayCircle size={20} />
+        </section>
       </div>
       <div className="flex flex-row items-center gap-4">
-        {song.rank === song.prevRank ? (
-          <FaCircle size={8} />
-        ) : song.rank > song.prevRank ? (
-          <AiOutlineCaretUp color="#3CA63F" />
-        ) : (
-          <AiOutlineCaretDown color="FF0000" />
-        )}
+        <div>
+          {song.rank === song.prevRank ? (
+            <FaCircle size={10} />
+          ) : song.rank > song.prevRank ? (
+            <AiOutlineCaretUp size={10} color="#3CA63F" />
+          ) : (
+            <AiOutlineCaretDown size={10} color="FF0000" />
+          )}
+        </div>
+        <div>
+          {song.rank + 1}
+        </div>
+        <div>
+          {song.name}
+        </div>
       </div>
+      <section className="hidden group-hover:flex absolute top-0 right-0 flex-row justify-end items-center h-[48px] w-1/2 bg-[rgba(0,0,0,0.7)]">
+        <IconButton icon={<FiThumbsDown size={20} />} />
+        <IconButton icon={<FiThumbsUp size={20} />} />
+        <IconButton icon={<FiMoreVertical size={20} />} />
+      </section>
     </article>
   );
 }
