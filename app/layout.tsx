3e8ef04fb104aa6610062ac4f303658669785b04
seongjin96 <w8348@naver.com>
2024-06-12 10:19:18 +0900

    fix theme hydration error

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body
+        // https://velog.io/@dudwns/next-themes%EB%A1%9C-%EB%8B%A4%ED%81%AC-%EB%AA%A8%EB%93%9C-%EA%B5%AC%ED%98%84-%EC%8B%9C-Extra-attributes-from-the-server-class-style-%EC%97%90%EB%9F%AC
+        suppressHydrationWarning={true}
+        className={inter.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
