a3cad4a1f37a61ea4eb3f4419cd604fedfb08486
seongjin96 <w8348@naver.com>
2024-06-13 17:07:15 +0900

    add SongCardRowExpand component

--- app/playlist/page.tsx
+++ app/playlist/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import {getPlaylistById} from "@/lib/dummyData";
-import {Playlist} from "@/types";
+import {Playlist, Song} from "@/types";
 import {permanentRedirect} from "next/navigation";
 import {getRandomElementFromArray} from "@/lib/utils";
 import HeaderBgChanger from "@/components/HeaderBgChanger";
 import PagePadding from "@/components/PagePadding";
 import PlayListHead from "@/components/PlayListHead";
+import SongCardRowExpand from "@/components/SongCardRowExpand";
 
 interface PlaylistPageProps {
   searchParams: {
@@ -30,6 +31,14 @@ const page = async (props: PlaylistPageProps) => {
         playlistName={playlist.playlistName}
         songList={playlist.songList}
       />
+      <div className="mt-12"></div>
+      <section className="flex flex-col gap-2">
+        {
+          playlist.songList.map((song: Song, idx: number) => {
+            return <SongCardRowExpand song={song} key={idx} />;
+          })
+        }
+      </section>
     </PagePadding>
   );
 }
