ea1a0932c14ab2d384cce46bd0184253adf4992c
seongjin96 <w8348@naver.com>
2024-06-20 15:43:29 +0900

    add usePlayerState using zustand

--- hooks/usePlayerState.ts
+++ hooks/usePlayerState.ts
@@ -1,13 +1,64 @@
 import {create} from "zustand";
+import {Song} from "@/types";
 
 interface PlayerState {
   isVisiblePlayer: boolean;
   setIsVisiblePlayer: (isVisiblePlayer: boolean) => void;
+  activeSong?: Song | null;
+  prevPlayerQueue: Song[];
+  nextPlayerQueue: Song[];
+  // 기능들(재생, 다음곡, 이전곡)
+  addSongList: (songList: Song[]) => void;
+  playNext: () => void;
+  playBack: () => void;
 }
 
 const usePlayerState = create<PlayerState>((set) => ({
   isVisiblePlayer: true,
   setIsVisiblePlayer: (isVisiblePlayer: boolean) => set({ isVisiblePlayer }),
+  activeSong: null,
+  prevPlayerQueue: [],
+  nextPlayerQueue: [],
+  addSongList: (songList: Song[]) =>
+      set((prev: PlayerState) => {
+      const prevSong: Song | null | undefined = prev.activeSong;
+      const cloneSongList: Song[] = [...songList];
+      const currentSong: Song = cloneSongList.splice(0, 1)?.[0];
+
+      return {
+        activeSong: currentSong,
+        prevPlayerQueue: prevSong
+          ? [prevSong, ...prev.prevPlayerQueue]
+          : prev.prevPlayerQueue,
+        nextPlayerQueue: [...cloneSongList],
+      };
+    }),
+  playNext: () => set((prev: PlayerState) => {
+    const currentSong: Song | null | undefined = prev.activeSong;
+    const nextSrc: Song = prev.nextPlayerQueue.splice(0, 1)?.[0];
+
+    return {
+      activeSong: nextSrc,
+      nextPlayerQueue: prev.nextPlayerQueue,
+      prevPlayerQueue: [
+        ...(currentSong ? [currentSong] : []),
+        ...prev.prevPlayerQueue
+      ],
+    }
+  }),
+  playBack: () => set((prev: PlayerState) => {
+    const currentSong: Song | null | undefined = prev.activeSong;
+    const preSrc: Song = prev.prevPlayerQueue.splice(0, 1)?.[0];
+
+    return {
+      activeSong: preSrc,
+      nextPlayerQueue: [
+        ...(currentSong ? [currentSong] : []),
+        ...prev.nextPlayerQueue
+      ],
+      prevPlayerQueue: prev.prevPlayerQueue
+    }
+  })
 }))
 
 export default usePlayerState;
