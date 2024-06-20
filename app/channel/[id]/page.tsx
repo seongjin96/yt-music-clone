6ca82039df1526e9776fd64979862cffe54c9d7d
seongjin96 <w8348@naver.com>
2024-06-20 12:04:05 +0900

    add song & album components in Channel Page

--- app/channel/[id]/page.tsx
+++ app/channel/[id]/page.tsx
@@ -1,12 +1,14 @@
 import {getChannelById} from "@/lib/dummyData";
 import {permanentRedirect} from "next/navigation";
-import {Channel} from "@/types";
+import {Channel, Song} from "@/types";
 import PagePadding from "@/components/PagePadding";
 import HeaderBgChanger from "@/components/HeaderBgChanger";
 import {getRandomElementFromArray} from "@/lib/utils";
 import DarkButton from "@/components/elements/DarkButton";
 import {FiMusic, FiShuffle} from "react-icons/fi";
 import WhiteButton from "@/components/elements/WhiteButton";
+import SongCardRowExpand from "@/components/SongCardRowExpand";
+import PlayListCarousel from "@/components/PlayListCarousel";
 
 interface ChannelPageProps {
   params: {
@@ -41,7 +43,7 @@ const page = async (props: ChannelPageProps) => {
             />
             <WhiteButton
               label={"뮤직 스테이션"}
-              Icon={<FiMusic size={16} />}
+              Icon={<FiMusic size={16}/>}
             />
           </div>
         </article>
@@ -52,7 +54,7 @@ const page = async (props: ChannelPageProps) => {
           />
           <WhiteButton
             label={"뮤직 스테이션"}
-            Icon={<FiMusic size={16} />}
+            Icon={<FiMusic size={16}/>}
           />
           <DarkButton
             className={"w-[230px] flex justify-center"}
@@ -60,12 +62,23 @@ const page = async (props: ChannelPageProps) => {
           />
         </div>
       </section>
-      <section>
-        노래
+      <section className="mt-[80px]">
+        <div className="text-[28px] font-bold">노래</div>
+        <div className="mt-20px">
+          <ul className="flex flex-col gap-2">
+            {channel.songList.map((song: Song, key: number) => {
+              return <SongCardRowExpand song={song} key={key} />;
+            })}
+          </ul>
+        </div>
       </section>
-      <section>
-        앨범
+      <section className="mt-[80px]">
+        <div className="text-[28px] font-bold">앨범</div>
+        <PlayListCarousel
+          playlistArray={channel.playlistArray}
+        />
       </section>
+      <section className="mt-[80px]"></section>
     </PagePadding>
   );
 }
